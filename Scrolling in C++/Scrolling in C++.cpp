9583591f16a575b5dc0a48c93e99d64ece196d1b
MoreToDo <donald_ricks@hotmail.com>
2016-11-17 23:31:45 -0800

    Gathered this from https://www.youtube.com/watch?v=u68ouf-kqQ4

--- Scrolling in C++/Scrolling in C++.cpp
+++ Scrolling in C++/Scrolling in C++.cpp
@@ -2,10 +2,84 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
 
 int main()
 {
+
+   string Menu[3] = { "Start Game\n", "Options\n", "Exit\n" };
+   int pointer = 0;
+
+   while (true)
+   {
+      system("cls");
+
+      SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); //set the color to 15 white
+      cout << "Main Menu\n\n";
+
+      for (int i = 0; i < 3; i++)
+      {
+         if (i == pointer)
+         {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11); // set the color to green
+            cout << Menu[i] << endl;
+         }
+         else
+         {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); //set the color to 15 white
+            cout << Menu[i] << endl;
+         }
+      }
+      while (true)
+      {
+         if (GetAsyncKeyState(VK_UP) != 0)
+         {
+            pointer -= 1;
+            if (pointer == -1)
+            {
+               pointer = 2;
+            }
+            break;
+         }
+         else if (GetAsyncKeyState(VK_DOWN) != 0)
+         {
+            pointer += 1;
+            if (pointer == 3)
+            {
+               pointer = 0;
+            }
+            break;
+         }
+         else if (GetAsyncKeyState(VK_RETURN) != 0)
+         {
+            switch (pointer) 
+            { 
+            case 0:
+               {
+                  cout << "\n\n\nStarting New Game!";
+                  Sleep(1000);
+               }
+               break;
+            case 1:
+               {
+               cout << "\n\n\nThe option menu!!";
+               Sleep(1000);
+               }
+               break;
+            case 2:
+               {
+               return 0;
+            }break;
+            }
+         }break;
+      }
+      Sleep(150);
+   }
+
+   system("pause");
     return 0;
 }
 
